5d4c89fd945226044a15c43cf145a1771a1cf194
Lucas Zimmerman <1445627@brunel.ac.uk>
2018-10-05 16:29:26 +0100

    first commit

---
+++ class1.py
@@ -0,0 +1,44 @@
+#BELOW IS GOOD CODE
+#print("Better than Fergus")
+
+#variables
+#p = 14
+#t = 0.12
+#price = p+p*t
+#print ("£", price)
+#if 5<6:
+#    print("great")
+#if p*t<16:
+#    print("bought")
+
+#strings
+#age = 22
+#height = 1.85
+#name = "Lucas"
+
+#print("Your name is %s, your age is %d and you are %.2f m tall" % (name, age, height))
+#print(name[:3])
+
+
+#input
+#name = input("what is your name?")
+#age = int( input("How old are you?"))
+#input("what is your name?")
+#input("How old are you?")
+#print ("Your name is %s and you are %d years old" % (name, age))
+
+
+#conditionals
+number = int( input("Pick a random number: "))
+
+if (number) <= 100 :
+    print ("That's a small number.")
+elif number <= 1000:
+    print ("That's not a big number yet.")
+else:
+    print ("That's an adequately huge number!")
+
+if (number%2)==1:
+    print ("This is an odd number.")
+else:
+    print ("This is an even number.")
