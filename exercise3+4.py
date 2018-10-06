f69d45ffdc3f3419f737a411e570ae5e7a0c2d85
LZZimmerman <43885846+LZZimmerman@users.noreply.github.com>
2018-10-06 12:07:20 +0100

    exercise5 (homework)

--- exercise3+4.py
+++ exercise3+4.py
@@ -1,14 +1,12 @@
-#Exercises 3+4
-number = int( input("Pick a random number: "))
-
-if (number) <= 100 :
-    print ("That's a small number.")
-elif number <= 1000:
-    print ("That's not a big number yet.")
+number = int( input("Give me a number: "))
+if number <=100:
+    print("That's a small number.")
+elif number <=1000:
+    print("That's not a big number yet.")
 else:
-    print ("That's an adequately huge number!")
+    print("That's an adequately huge number!")
 
-if (number%2)==1:
+if number%2 == 1:
     print ("This is an odd number.")
 else:
     print ("This is an even number.")
---
+++ exercise5.py
@@ -0,0 +1,20 @@
+#Problem solving V: Given a string, if its length is at least 3,
+#add 'ing' to its end. Unless it already ends in 'ing', in which
+#case add 'ly' instead. If the string length is less than 3,
+#leave it unchanged. Print the resulting string.
+word = input("Pick a random word: ")
+#print (word[-3:])
+#print (word + "ly")
+
+if (len(word) >=3 and word[-3:] != "ing"):
+    print (word + "ing")
+elif (len(word) >=3 and word[-3:] == "ing"):
+    print (word + "ly")
+else:
+    print (word)
+
+#or
+#elif (len(word) <3):
+#    print (word)
+#else:
+#    print (word + "ly")
