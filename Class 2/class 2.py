3983d82c8ce4ea27ec178d0315136df82990a75d
LZZimmerman <1445627@brunel.ac.uk>
2018-10-19 15:20:36 +0100

    Update Class 2/class 2.py

---
+++ Class 2/class 2.py
@@ -0,0 +1,43 @@
+'''
+count = 0
+mohammad_computer_work = False
+
+while count <8:
+    print(count)
+
+    if count ==6:
+        print ("syke Mohammad")
+        break
+    else:
+        count += 1
+        continue
+for i in range(20,41,2):
+    print(i)
+
+count2 = 0
+
+while mohammad_computer_work == False:
+    print ("haha")
+
+    if count2 == 10:
+        print (":(")
+        break
+    else:
+        count2+=1
+        continue
+'''
+
+'''
+word = "python"
+for i in (word):
+    if i == "t":
+        continue
+    #if i != "t":
+    print(i, end="")
+'''
+
+
+
+
+
+
