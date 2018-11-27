0245f187fa7ff0a79568daeb6940f14b6c8b5e89
LZZimmerman <43885846+LZZimmerman@users.noreply.github.com>
2018-11-27 19:38:17 +0000

    listhw1

---
+++ Homework(Week6)/ListExercise1.py
@@ -0,0 +1,15 @@
+### Exercise 1 ### -------------------------------------------------
+
+# list1 = [3,4,4,5,5,6]
+list1 = [1,0,0,0,2,8,11]
+newList1 = []
+
+for i in list1:
+    if i not in newList1:
+        newList1.append(i)
+
+
+print(newList1)
+
+
+
